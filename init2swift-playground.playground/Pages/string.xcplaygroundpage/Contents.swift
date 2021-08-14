298e083b66bd7d636b790f3816324801e65c5578
Allansrc <allanramos.teixeira@gmail.com>
2021-08-13 22:56:10 -0300

    update String playground

--- init2swift-playground.playground/Pages/string.xcplaygroundpage/Contents.swift
+++ init2swift-playground.playground/Pages/string.xcplaygroundpage/Contents.swift
@@ -2,6 +2,20 @@
 
 import Foundation
 
-var greeting = "Hello, playground"
+var greeting = "Hello, from a String"
+
+let anConstantString = "I'm a constant"
+
+// should not work
+//anConstantString = "should I take a new value?"
+
+greeting = "Hello World from a Non-constant String"
+
+var strName = "Allan"
+//print(strName.reversed())
+
+for char in strName.reversed() {
+    print(char)
+}
 
 //: [Next](@next)
