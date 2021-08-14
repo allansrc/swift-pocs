280e51cb6bd9816e3b470622b93610f892dd158e
Allansrc <allanramos.teixeira@gmail.com>
2021-08-13 22:16:45 -0300

    Initial Commit

---
+++ Shared/init2swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  init2swiftApp.swift
+//  Shared
+//
+//  Created by Allan Ramos on 13/08/21.
+//
+
+import SwiftUI
+
+@main
+struct init2swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
